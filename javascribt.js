da6a995797c1ea502a58487d213aa667fc4a8040
karimsarhan <83120668+KarimSarhancsfd@users.noreply.github.com>
2024-10-24 19:52:59 +0300

    adidas-inspiration
    this design was inspired by the official website of Adidas. I hope you enjoy using my code.

---
+++ javascribt.js
@@ -0,0 +1,4 @@
+// Placeholder for future dynamic functionality
+document.querySelector('.search-cart input').addEventListener('input', function(e) {
+    console.log('Search query:', e.target.value);
+});
